69c39dab8d84747c4c379211d343d079b7a9bb39
Juliana Schwartz <juliana.schwartz@hotmail.com>
2020-05-21 15:22:44 -0300

    Projeto Pronto

---
+++ src/entitites/Rectangle.java
@@ -0,0 +1,18 @@
+package entitites;
+
+public class Rectangle {
+	
+	public double width, height;
+	
+	public double area() {
+		return width * height;
+	}
+	
+	public double perimeter() {
+		return (2 * width) + (2 * height);
+	}
+	
+	public double diagonal() {
+		return Math.sqrt((Math.pow(width, 2) + Math.pow(height, 2)));
+	}
+}
